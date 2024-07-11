d4cd3c531bf268e8dc59d8bbd10eed087b4a9e2c
Guillaume RAK-LECLER <56916245+gRAKLECLER@users.noreply.github.com>
2024-07-11 12:12:42 +0200

    test commit

--- src/app/Components/Header/Header.tsx
+++ src/app/Components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link"
 import "../Header/Header.css"
+import Image from "next/image"
 
 const Header = () => {
     return(
         <header className="header">
             <Link href={'/'}>
-                <img src="http://www.forest-hill.pro/wp-content/uploads/2017/12/logo-mobile-vert.png" alt="" />
+                <Image src="http://www.forest-hill.pro/wp-content/uploads/2017/12/logo-mobile-vert.png" alt="" width={300} height={150}/>
             </Link>
             <ul className="header_links">
                 <Link href={'/'}>ACCUEIL</Link>
